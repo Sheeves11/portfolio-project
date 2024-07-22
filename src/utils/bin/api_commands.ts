d03780f07099a07ad0505dda49cb5d6201a763fa
Mia Quain <miaquain111@gmail.com>
2024-07-22 17:54:17 -0500

    Update api_commands.ts

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -3,7 +3,7 @@
 import { getProjects } from '../api';
 import { getQuote } from '../api';
 import { getReadme } from '../api';
-import { getWeather } from '../api';
+//import { getWeather } from '../api';
 
 export const projects = async (args: string[]): Promise<string> => {
   const projects = await getProjects();
@@ -26,11 +26,11 @@ export const readme = async (args: string[]): Promise<string> => {
   ${readme}`;
 };
 
-export const weather = async (args: string[]): Promise<string> => {
-  const city = args.join('+');
-  if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
-  }
-  const weather = await getWeather(city);
-  return weather;
-};
+// export const weather = async (args: string[]): Promise<string> => {
+//   const city = args.join('+');
+//   if (!city) {
+//     return 'Usage: weather [city]. Example: weather casablanca';
+//  }
+//  const weather = await getWeather(city);
+//  return weather;
+//};
