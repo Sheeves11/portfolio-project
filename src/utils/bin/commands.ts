2629282cca8b0162583af3f5c6b810b8a33dbf0e
Mia Quain <miaquain111@gmail.com>
2024-07-22 18:38:29 -0500

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -198,6 +198,8 @@ If not, feel free to explore!
 Thanks for looking,
 Mia Quain <3
 
+--------------------------------------------------------
+
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display a summary.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
