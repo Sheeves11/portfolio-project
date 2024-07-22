fc3b43cc775e97d1875880e70d9da89812a15872
Mia Quain <miaquain111@gmail.com>
2024-07-22 11:25:55 -0500

    remove unused constants

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -8,20 +8,6 @@ export const getProjects = async () => {
   return data;
 };
 
-export const getReadme = async () => {
-  const { data } = await axios.get(config.readmeUrl);
-  return data;
-};
-
-export const getWeather = async (city: string) => {
-  try {
-    const { data } = await axios.get(`https://wttr.in/${city}?ATm`);
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
-
 export const getQuote = async () => {
   const { data } = await axios.get('https://api.quotable.io/random');
   return {
