0f7beb126eba2cda42238c6474bb09e989fa271b
Mia Quain <miaquain111@gmail.com>
2024-07-22 17:57:40 -0500

    Update api_commands.ts

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,30 +1,30 @@
 // // List of commands that require API calls
 
-import { getProjects } from '../api';
-import { getQuote } from '../api';
-import { getReadme } from '../api';
+//import { getProjects } from '../api';
+//import { getQuote } from '../api';
+//import { getReadme } from '../api';
 //import { getWeather } from '../api';
 
-export const projects = async (args: string[]): Promise<string> => {
-  const projects = await getProjects();
-  return projects
-    .map(
-      (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
-    )
-    .join('\n');
-};
+//export const projects = async (args: string[]): Promise<string> => {
+//  const projects = await getProjects();
+//  return projects
+//    .map(
+//      (repo) =>
+//        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
+//    )
+//    .join('\n');
+//};
 
-export const quote = async (args: string[]): Promise<string> => {
-  const data = await getQuote();
-  return data.quote;
-};
+//export const quote = async (args: string[]): Promise<string> => {
+//  const data = await getQuote();
+//  return data.quote;
+//};
 
-export const readme = async (args: string[]): Promise<string> => {
-  const readme = await getReadme();
-  return `Opening GitHub README...\n
-  ${readme}`;
-};
+//export const readme = async (args: string[]): Promise<string> => {
+//  const readme = await getReadme();
+//  return `Opening GitHub README...\n
+//  ${readme}`;
+//};
 
 // export const weather = async (args: string[]): Promise<string> => {
 //   const city = args.join('+');
