67d22866e6c1b852b2fccab3a3b5e7950867b6a7
Mia Quain <miaquain111@gmail.com>
2024-07-22 22:07:02 -0500

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -7,24 +7,24 @@ const sumfetch = async (args: string[]): Promise<string> => {
   } else {
     return `
 
-⠀⠀⣿⠛⢳⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣔⢁⡁⡆⠀                Mia Quain
-⠀⢰⣽⡆⠀⠈⠎⣡⢆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠑⡈⠃⣠⡿⡇⡇⠀              -----------
+⠀⠀⣿⠛⢳⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣔⢁⡁⡆⠀               Mia Quain
+⠀⢰⣽⡆⠀⠈⠎⣡⢆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠑⡈⠃⣠⡿⡇⡇⠀             -----------
 ⠀⠸⣕⣿⣇⣀⡀⠉⠉⠻⠞⠜⠳⠘⡒⡒⡔⣦⡠⣄⣀⢔⠥⠅⠁⠀⣐⣿⣟⣻⡅⠀
 ⠀⠻⡓⣿⣻⣿⣇⢆⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠁⠀⡀⠄⣮⣝⢏⡏⡃⠀              INFO
 ⠀⠨⢍⣬⡯⣟⢿⣿⡖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣥⣿⢻⢯⢿⠃⠅⠀              -----------
-⠀⠀⢃⢕⣧⣗⡁⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⢛⢽⣿⡿⡅⠀⠀                <u><a href="${config.resume_url}" target="_blank">resume.pdf</a></u>
-⠀⠀⠀⢙⡷⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢸⣫⢉,⠄                 phone: 479.787.0038
-⠀⠀⢀⡺⡇⠀⠀⣀⣂⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⣀⣀⣀⡀⡀⣀⠀⠀⠁⢯⡡⠅⠀               email: <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+⠀⠀⢃⢕⣧⣗⡁⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⢛⢽⣿⡿⡅⠀⠀               <u><a href="${config.resume_url}" target="_blank">resume.pdf</a></u>
+⠀⠀⠀⢙⡷⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢸⣫⢉,⠄                phone: 479.787.0038
+⠀⠀⢀⡺⡇⠀⠀⣀⣂⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⣀⣀⣀⡀⡀⣀⠀⠀⠁⢯⡡⠅⠀              email: <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ⠀⠀⡰⠇⡅⠀⠀⠈⠉⠻⠧⠗⠉⠀⠀⠀⠀⠀⠀⠛⠯⠻⠋⠁⠀⠀⠀⠐⢛⣿⠇⠀
-⠐⠐⡋⠄⢁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⡻⡕⠂                 SOCIAL
-⠠⢀⡶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣽⣄⣔⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣻⠪⡀               -----------
-⠀⠀⡽⣶⡆⠅⠄⠀⠀⠀⠀⠀⠀⠀⢹⣏⠏⠁⠀⠀⠀⠀⠀⠀⠀⠀⡰⢢⢟⡿⡻⠀                <u><a href="instagram.com/pebbleandbraid}" target="_blank">instagram.com/pebbleandbraid}</a></u>
-⠀⠀⠀⠐⠁⢝⢄⡀⠀⠀⠀⠀⠀⠀⣠⣏⡀⠀⠀⠀⠀⠀⠀⠀⢀⠀⢀⣾⠧⠉"                  <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-⠀⠀⠀⠂⠈⡢⠗⣿⡆⣦⢀⣀⣔⡮⠘⠑⠻⢷⣶⣆⣀⢀⢃⡤⡦⡿⡎⠚                   <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+⠐⠐⡋⠄⢁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⡻⡕⠂                SOCIAL
+⠠⢀⡶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣽⣄⣔⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣻⠪⡀              -----------
+⠀⠀⡽⣶⡆⠅⠄⠀⠀⠀⠀⠀⠀⠀⢹⣏⠏⠁⠀⠀⠀⠀⠀⠀⠀⠀⡰⢢⢟⡿⡻⠀               <u><a href="instagram.com/pebbleandbraid}" target="_blank">instagram.com/pebbleandbraid}</a></u>
+⠀⠀⠀⠐⠁⢝⢄⡀⠀⠀⠀⠀⠀⠀⣠⣏⡀⠀⠀⠀⠀⠀⠀⠀⢀⠀⢀⣾⠧⠉"                <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+⠀⠀⠀⠂⠈⡢⠗⣿⡆⣦⢀⣀⣔⡮⠘⠑⠻⢷⣶⣆⣀⢀⢃⡤⡦⡿⡎⠚                 <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
 ⠀⠀⠀⠀⠀⠁⠀⠀⠈⡷⣌⣠⡀⡀⠀⠀⠀⢀⢌⡏⢓⠫⠫⠁⠁⠄⠁⠈⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠂⠒⠂⠉⠑⠪⠣⠇⠏⠛⠉⠈⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀                   ART & DESIGN
-                                                         <u><a href="https://www.behance.net/sheeves11" target="_blank">Behance Portfolio</a></u>
-                                                         <u><a href="${config.repo}" target="_blank">this website's source code</a></u>
+⠀⠀⠀⠀⠀⠀⠀⠀⠂⠒⠂⠉⠑⠪⠣⠇⠏⠛⠉⠈⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀                 ART & DESIGN
+                                                      <u><a href="https://www.behance.net/sheeves11" target="_blank">Behance Portfolio</a></u>
+                                                      <u><a href="${config.repo}" target="_blank">this website's source code</a></u>
 
                                             
 `;
