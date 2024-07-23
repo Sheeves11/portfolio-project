9ca44121d866088fd2057de7d764ffc76b1ea103
Mia Quain <miaquain111@gmail.com>
2024-07-22 21:31:05 -0500

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -8,19 +8,19 @@ const sumfetch = async (args: string[]): Promise<string> => {
     return `
            ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  Mia Quain
         ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
-      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
-    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
-▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Source Code</a></u>
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             INFO
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="${config.resume_url}" target="_blank">resume.pdf</a></u>
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           phone: 479.787.0038
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓        email: <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓                                 ▐▓       -----------
-▐▓             > M I A             ▐▓        CONTACT 
-▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓             > M I A             ▐▓        SOCIAL 
+▐▓                                 ▐▓        <u><a href="instagram.com/pebbleandbraid}" target="_blank">instagram.com/pebbleandbraid}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             ART 
-        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
-            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  爵 <u><a href="https://www.behance.net/sheeves11" target="_blank">Behance Portfolio</a></u>
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             ART & DESIGN
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="https://www.behance.net/sheeves11" target="_blank">Behance Portfolio</a></u>
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.repo}" target="_blank">this website's source code</a></u>
 
 `;
   }
